4fa6a2504c58ba9561065019149a8b733193e1cd
Eduardo <edo.rod.san@gmail.com>
2024-02-03 03:22:02 -0600

    update front

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,85 @@
-function About () {
+function Badge({ children }: { children: string }) {
   return (
     <div>
-      <h1>About</h1>
+      <span className=" p-1 text-xs font-medium rounded-md bg-gray-100 text-gray-800">
+        {children}
+      </span>
     </div>
-  )
+  );
 }
 
-export { About }
+function CardMember({
+  name,
+  description,
+  children,
+}: {
+  name: string,
+  description: string,
+  children?: string,
+}) {
+  return (
+    <div className="flex gap-4">
+      <div className="w-[300px] h-[200px] overflow-hidden rounded-lg">
+        <img
+          src="https://images.unsplash.com/photo-1706773183831-223a782307ec?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt={name}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h3 className="text-3xl font-medium">{name}</h3>
+        {children}
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="w-[1200px] mx-auto flex flex-col  gap-4 items-center">
+      <div className="w-[800px] rounded-3xl overflow-hidden">
+        <img
+          src="/recursos/logo-shot-2.png"
+          alt="JEL"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <p className="text-2xl">
+        The name <strong>JEL</strong> comes from the acronyms of our names{" "}
+        <strong>J</strong>onathan, <strong>E</strong>duardo and{" "}
+        <strong>L</strong>uis.
+      </p>
+
+      <div className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold">Members</h2>
+        <CardMember
+          name="Jonathan"
+          description="Jonathan is a computer science student at the University of Havana. He
+          is passionate about programming and technology. He is the front-end
+          developer of the team."
+        >
+          <Badge>Front-end developer</Badge>
+        </CardMember>
+        <CardMember
+          name="Luis"
+          description="Jonathan is a computer science student at the University of Havana. He
+          is passionate about programming and technology. He is the front-end
+          developer of the team."
+        >
+          <Badge>Front-end developer</Badge>
+        </CardMember>
+        <CardMember
+          name="Eduardo"
+          description="Jonathan is a computer science student at the University of Havana. He
+          is passionate about programming and technology. He is the front-end
+          developer of the team."
+        >
+          <Badge>Front-end developer</Badge>
+        </CardMember>
+      </div>
+    </div>
+  );
+}
+
+export { About };
--- src/pages/vote/Vote.jsx
+++ src/pages/vote/Vote.jsx
@@ -1,9 +1,36 @@
-function Vote () {
+function Surveys() {
   return (
-    <div>
-      Vote
+    <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+      <ul className="flex flex-wrap -mb-px">
+        <li className="me-2">
+          <a
+            href="/"
+            className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
+            aria-current="page"
+          >
+            Dashboard
+          </a>
+        </li>
+      </ul>
     </div>
+  );
+}
+
+function Survey () {
+  return (
+    <div className="w-[1200px] mx-auto flex flex-col gap-4">
+      <Surveys />
+    </div>
+
   )
+}
+
+function Vote() {
+  return (
+    <div className="w-[1200px] mx-auto flex flex-col gap-4">
+      {/* <Surveys /> */}
+    </div>
+  );
 }
 
 export { Vote };
